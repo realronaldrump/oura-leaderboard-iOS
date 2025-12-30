2b88fedd15f19670566feb17e1a60e8dededaa03
davisdeatonphotography <davis.deaton@verizon.net>
2025-12-30 12:37:21 -0700

    Initial Commit

---
+++ oura-leaderboard-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  oura-leaderboard-iOS
+//
+//  Created by Davis Deaton on 12/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ oura-leaderboard-iOS/oura_leaderboard_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  oura_leaderboard_iOSApp.swift
+//  oura-leaderboard-iOS
+//
+//  Created by Davis Deaton on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct oura_leaderboard_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
